ec6580ca60de22086dfaf3028d777fe172a9c6df
Naufalhv <143594831+Naufalhv@users.noreply.github.com>
2023-09-05 09:00:12 +0700

    Add files via upload

---
+++ Naufal Rifqi Hafizuddin/Latihan2/src/latihan2/Latihan2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan2;
+
+/**
+ *
+ * @author computer 20
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=15;
+        int y=20;
+        int z=30;
+        
+        int rata2=(x+y+z)/3;
+         System.out.println(rata2);
+         
+        float rata3=(x+y+z)/3;
+         System.out.println(rata3);
+         
+        String rata4=String.valueOf(rata2);
+          System.out.println(rata4);
+        
+    }
+    
+}
---
+++ Naufal Rifqi Hafizuddin/Latihan2/src/latihan2/Latihan2_1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan2;
+
+/**
+ *
+ * @author computer 20
+ */
+public class Latihan2_1 {
+    
+}
